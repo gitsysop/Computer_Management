55f4498e01a94e0b5f8f3885c340dba2e64eb433
gitsysop <lluuxxoorr@gmail.com>
2022-02-13 21:03:21 +0100

    scaffold identity

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseSqlServer("DefaultConnection"));
 
 builder.Services.AddDbContext<CMContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("default")));
